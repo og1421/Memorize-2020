9c3f0100cfa576ee9a362856bd916cba144858dd
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-20 14:42:57 -0300

    Initial Commit

---
+++ Memorize 2020/Memorize_2020App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_2020App.swift
+//  Memorize 2020
+//
+//  Created by Orlando Moraes Martins on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_2020App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
